da6d2172cb7be46545e52307f560d359be2001fa
Hiramnam <hiramnm14@gmail.com>
2023-06-15 10:12:00 -0600

    Prueba de github Pages

---
+++ src/fetch.js
@@ -0,0 +1,43 @@
+// const BASE_URL = 'https://pokeapi.co/api/v2/';
+
+// const pokeContainer = document.getElementById('pokemon-container');
+
+// pokeContainer.addEventListener('load', async () => {
+//     fetchPokemon();
+// })
+
+// btnFetch.addEventListener('click', async () => {
+//     const pokemon = await fetchPokemon();
+
+//     pokemon.results.forEach(async (pokemon) => {
+
+//     })
+//     console.log(pokemon.results);
+// } )
+
+// const fetchPokemon = async () => {
+// //     const data = await fetch(BASE_URL + 'pokemon?limit=151'); 
+// //     const parsedData = await data.json();
+// //     console.log(parsedData.results);
+// // return parsedData;
+
+// try {
+
+//     const data = await fetch(BASE_URL + 'pokemon?limit=151');
+//     const parsedData = await data.json();
+//     return parsedData;
+
+// } catch (err) {
+// console.error(err);
+// }
+
+// }
+
+// fetchPokemon();
+
+
+
+
+
+
+
---
+++ src/main.js
@@ -0,0 +1,77 @@
+// async function f(){
+
+// let promise = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve('Exito'), 3000);
+// })
+// const result = await promise;
+// alert(promise);
+// }
+
+// Simulacion de una peticion asincrona
+
+// async function f() {
+//     let promise = new Promise((res, rej) => {
+//         setTimeout(() => res('done'), 3000)
+//     })
+    
+//     let result = await promise;
+    
+//     alert(result);
+// }
+
+// f();
+
+
+/*
+const btn = document.getElementById('btn-count');
+const p = document.getElementById('contador');
+
+let counter = 0;
+
+btn.addEventListener('click', async () => {
+    await setTimeout(() => {
+        counter++;
+        p.textContent = counter;
+    }, 1000
+    );
+
+});
+
+
+p.textContent = counter;
+
+const btn2 = document.getElementById('btn2-rest');
+const b = document.getElementById('restador');
+
+let rest = 0;
+
+btn2.addEventListener('click', async () => {
+    await setTimeout(() => {
+        rest--;
+        p.textContent = counter;
+    }, 1000
+    );
+
+});
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
